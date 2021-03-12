8d2fe57bab17d8473f51db8d7f886c33a34159bc
Blake Madigan <blakemadigan@192-168-1-152.tpgi.com.au>
2021-03-12 21:46:12 +1100

    Taking Decisions: IF/ELSE Statments

--- The Complete JavaScript Course 2020/complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ The Complete JavaScript Course 2020/complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -168,9 +168,28 @@ console.log(jonas);
 const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
 console.log(jonasNew);
 
-console.log(`Just a regular string...`)
+console.log(`Just a regular string...`);
 
 console.log(`string
 multiple
-lines`)
+lines`);
 */
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :) `);
+}
+
+const birthYear = 2012;
+
+let century;
+if(birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
